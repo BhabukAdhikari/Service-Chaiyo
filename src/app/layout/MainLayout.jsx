3264029fa41971a37f7fa3c6a35f8de25e75e89f
Bikash Ghimire <bikashghimire@Bikashs-MacBook-Air.local>
2024-03-04 16:35:34 +0545

    navbar finished

--- src/app/layout/MainLayout.jsx
+++ src/app/layout/MainLayout.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 //components
-import { Navbar } from "../components";
-import { Header } from "../components";
+import { Header, TopBar } from "../components";
 
 export const MainLayout = ({ children }) => {
   return (
     <div>
-      <Navbar />
+      <TopBar />
       <Header />
       {children}
     </div>
---
+++ src/app/mock/data.js
@@ -0,0 +1,23 @@
+export const Header = [
+    {
+        id: 1,
+        title: 'Home',
+        to: '/',
+        className:'home'
+    },
+    {
+        id: 2,
+        title: 'About Us',
+        to: '/about-us'
+    },
+    {
+        id: 3,
+        title: 'Blog',
+        to: '/blog'
+    },
+    {
+        id: 4,
+        title: 'Contact',
+        to: '/contact'
+    },
+]
