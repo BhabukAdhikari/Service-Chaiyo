b7bf1f2fa651a4224a4c43bc5454c3eabe094502
Bk10ST <133320594+Bk10ST@users.noreply.github.com>
2024-03-06 01:08:40 -0800

    new compnents aded

---
+++ src/app/components/Routes/Routes.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Router } from 'react-router-dom'
+import Navbar from '../Navbar/Navbar'
+
+const Routes = () => {
+  return (
+    <div>
+        <Router>
+            <Navbar/>
+        </Router>
+    </div>
+  )
+}
+
+export default Routes
