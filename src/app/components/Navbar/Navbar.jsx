0e12bd9ba17b5ed1fb5de9c7893d1a03b400d6b8
Bk10ST <133320594+Bk10ST@users.noreply.github.com>
2024-04-01 22:25:46 -0700

    changes in navbar and mainlayout

--- src/app/components/Navbar/Navbar.jsx
+++ src/app/components/Navbar/Navbar.jsx
@@ -17,27 +17,6 @@ export const TopBar = () => {
   const { isLogined, isAuthorized } = useContext(AuthContext)
   
   return (
-<<<<<<< HEAD
-    <Navbar collapseOnSelect expand="lg" className="nav-bg">
-      <Container fluid>
-        <Navbar.Brand href="/"> <TfiHeadphoneAlt />
-          &nbsp; ServiceProvider</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="mx-auto">
-            {headerData.map((item, index) => (
-              <Nav.Link href={item.to} className={item.className} key={index}>{item.title}</Nav.Link>
-            ))}
-          </Nav>
-          <Nav className='auth-head'>
-            <Nav.Link href="/login">Login</Nav.Link>
-            <div className="vertical"></div>
-            <Nav.Link href="/register">SignUp</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-=======
     <AuthProvider>
       <Navbar collapseOnSelect expand="lg" className="nav-bg">
         <Container fluid>
@@ -65,6 +44,5 @@ export const TopBar = () => {
         </Container>
       </Navbar>
     </AuthProvider>
->>>>>>> fetBikash
   );
 };
--- src/app/layout/MainLayout.jsx
+++ src/app/layout/MainLayout.jsx
@@ -1,9 +1,4 @@
 import { React } from "react";
-<<<<<<< HEAD
-=======
-import { Footer, TopBar } from "../components";
-import React from "react";
->>>>>>> 2d48e9d6a972dc8baf485066a9ce6ca89cabffa9
 import { Footer, TopBar } from "../components";
 
 export const MainLayout = ({ children }) => {
